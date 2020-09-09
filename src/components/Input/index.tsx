48a4c0f39606532fa17bc04ef51ac4269254425b
Danilo Gujansky Facco <faccodanilo@gmail.com>
2020-09-09 01:19:35 +0100

    Sign in & Sign Up

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,33 @@
+import React, {InputHTMLAttributes, useRef ,useEffect} from 'react'
+import {Container} from './styles'
+
+import {useField} from '@unform/core'
+
+import {IconBaseProps} from 'react-icons'
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+    name:string;
+    icon:React.ComponentType<IconBaseProps>;
+}
+const Input: React.FC<InputProps> = ({name, icon:Icon, ...rest}) => {
+    const inputRef = useRef(null)
+    const {fieldName,defaultValue,error,registerField} = useField(name)
+
+    useEffect(()=>{
+        registerField({
+            name:fieldName,
+            ref: inputRef.current,
+            path:'value'
+        })
+    },[fieldName,registerField])
+
+    return(
+
+    <Container>
+    {Icon && <Icon size={20}/>}
+    
+    <input defaultValue={defaultValue} ref={inputRef} {...rest}/>
+    </Container>
+    )
+} 
+
+export default Input
